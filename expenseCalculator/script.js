23e5f1691be752185d61d66ba292a404d9b74876
Taanveer22 <taanveer469@gmail.com>
2025-05-19 18:16:22 +0600

    alhamdulliah expense calculator small project done

--- expenseCalculator/script.js
+++ expenseCalculator/script.js
@@ -1,1 +1,31 @@
-console.log('connected');
+console.log("connected");
+
+const totalExpense = document.getElementById("total-expense");
+const expenseField = document.getElementById("expense-field");
+const amountField = document.getElementById("amount-field");
+const insertBtn = document.getElementById("insert-btn");
+const rowContainer = document.getElementById("row-container");
+
+insertBtn.addEventListener("click", function (event) {
+  console.log(event);
+  console.log(event.target);
+
+  const expenseValue = expenseField.value;
+  const amountValue = Number(amountField.value);
+  console.log(expenseValue, amountValue);
+
+  const row = `
+    <div class="flex justify-between" data-amount = "${amountValue}">
+        <p>${expenseValue}</p>
+        <p>${amountValue}</p>
+    </div>
+    `;
+
+  rowContainer.innerHTML = rowContainer.innerHTML + row;
+
+  let totalExpenseValue = Number(totalExpense.innerText);
+  console.log(totalExpenseValue);
+  totalExpenseValue = amountValue + totalExpenseValue;
+
+  totalExpense.innerText = totalExpenseValue;
+});
